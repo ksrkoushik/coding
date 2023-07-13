17a17ead7aa4aa22fcca61a8a3f2ec7e528ebec0
Kotakonda <e40052131@rcinnet.rockwellcollins.com>
2023-07-13 09:19:17 +0530

    Expt-new

---
+++ c/p6.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+	char name[20] = "Trisha Krishnan";
+	printf("Her name is %s\n", name);
+	return 0;
+}
---
+++ cpp/p6.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+	char artist[20] = "Chiranjeevi";
+	printf("His name is %s\n", artist);
+	cout << "Welldone\n";
+	return 0;
+}
