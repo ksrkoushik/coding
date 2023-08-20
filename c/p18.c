af58f3bdf077586149c5635c5cb0b384e36ee102
ksrkoushik <ksrkoushik@gmail.com>
2023-08-20 21:13:03 +0530

    till p18

---
+++ c/p18.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main()
+{
+	double a,b,c,y1,y2,y3;
+	a = 4;
+	b = 12.25;
+	c = 0.0121;
+	y1 = sqrt(a);
+	y2 = sqrt(b);
+	y3 = sqrt(c);
+	printf("%.2f\n", y1);
+	printf("%.2f\n", y2);
+	printf("%.2f\n", y3);
+	return 0;
+}
---
+++ python/p18.py
@@ -0,0 +1,4 @@
+import math
+print(math.sqrt(4))
+print(math.sqrt(12.25))
+print(math.sqrt(0.0121))
