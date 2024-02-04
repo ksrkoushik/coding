7f15bd44c4138da96a3640eb57b88886ffd9c1c0
ksrkoushik <ksrkoushik@gmail.com>
2024-02-04 13:05:14 +0530

    new files

---
+++ zedex/c/p1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// my first c program
+int main(int argc, char* argv[])
+{
+	int distance = 100;
+	printf("you are %d miles away.\n", distance);
+	printf("You are fantastic\n");
+	return 0;
+}
