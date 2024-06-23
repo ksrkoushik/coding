b4107f0672f5b7f4a86c4dcd1592b99d2f67ead8
ksrkoushik <ksrkoushik@gmail.com>
2024-06-23 20:12:12 +0530

    new

---
+++ zedex/python/p40a.py
@@ -0,0 +1,10 @@
+class MyStuff():#class
+    def __init__(self):#constructor created using dunder method
+        self.tangerine = "And now a thousand years between"
+
+    def apple(self):#function
+        print("I AM CLASSY APPLES!")
+
+thing = MyStuff()#class instantiation and object creation
+thing.apple()#accessing function from within the object
+print(thing.tangerine)#printing
